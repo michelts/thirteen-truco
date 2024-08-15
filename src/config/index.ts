382ececaef8b4fdb78cf46addb2765d3627b02a0
Michel Sabchuk <michel@turbosys.com.br>
2024-08-15 09:39:31 -0300

    Create default deck of cards

---
+++ src/config/index.ts
@@ -0,0 +1,19 @@
+import { Deck, Card, Suit } from "@/core";
+
+export const defaultDeck = new Deck(Array.from(generateDefaultCards()));
+
+function* generateDefaultCards() {
+  for (const cardNumber of range(13)) {
+    for (const suit of range(4)) {
+      yield new Card(cardNumber, suit);
+    }
+  }
+}
+
+function* range(count: number) {
+  let currentIndex = 1;
+  while (currentIndex <= count) {
+    yield currentIndex;
+    currentIndex++;
+  }
+}
