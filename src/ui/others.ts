42e68a8f7f28cf31959b6118c2a54519f2ba7f9e
Michel Sabchuk <michel@turbosys.com.br>
2024-08-24 17:10:09 -0300

    Make sure game is rendered clockwise

--- src/ui/others.ts
+++ src/ui/others.ts
@@ -1,3 +1,6 @@
 export function renderOthers(items: string[]) {
-  return `<div id="ot"><div>${items.map((item) => `<div>${item}</div>`).join("")}</div></div>`;
+  return `<div id="ot"><div>${items
+    .reverse() // reverse so game runs clockwise
+    .map((item) => `<div>${item}</div>`)
+    .join("")}</div></div>`;
 }
