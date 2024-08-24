a46c5ed2afa83398fdbb2a370540abe8dfb1a1a2
Michel Sabchuk <michel@turbosys.com.br>
2024-08-24 18:43:04 -0300

    Drop cards using drag and drop and prepare card discarding

--- src/ui/myCards.ts
+++ src/ui/myCards.ts
@@ -23,19 +23,68 @@ function render(game: Game, player: Player) {
 
 function renderPlayerCard(game: Game, player: Player, card: Card) {
   const id = `mc-${card.cardNumber}-${card.suit}`;
-  setTimeout(() => {
-    getElement(id).addEventListener("dragend", (event) => {
-      const rect = (event.target as HTMLElement).getBoundingClientRect();
-      if (event.clientY < rect.top) {
-        dispatchEvent(cardDropped(game, player, card));
-      } else if (event.clientY > rect.top + rect.height) {
-        dispatchEvent(cardDropped(game, player, card));
-      }
-    });
-  });
-  return `<button id="${id}" draggable="true">${renderCard(card)}</button>`;
+  setTimeout(() => listenToEvents(game, player, card, id));
+  return `<button id="${id}">${renderCard(card)}</button>`;
 }
 
 function redraw(game: Game, player: Player) {
   getElement("mc").innerHTML = render(game, player);
+}
+
+function listenToEvents(game: Game, player: Player, card: Card, id: string) {
+  let isDragging = false;
+  let originalX = 0;
+  let originalY = 0;
+  let newLeft = 0;
+  let newTop = 0;
+  const element = getElement(id);
+  const rect = element.getBoundingClientRect();
+
+  function dragStart(event: { clientX: number; clientY: number }) {
+    isDragging = true;
+    originalX = event.clientX;
+    originalY = event.clientY;
+  }
+
+  function dragMove(event: { clientX: number; clientY: number }) {
+    if (!isDragging) {
+      return;
+    }
+    newLeft = event.clientX - originalX;
+    newTop = event.clientY - originalY;
+    element.style.left = `${newLeft}px`;
+    element.style.top = `${newTop}px`;
+  }
+
+  function dragCancel() {
+    isDragging = false;
+    newLeft = 0;
+    newTop = 0;
+    element.style.top = "0";
+    element.style.left = "0";
+  }
+
+  function dragEnd() {
+    // Drop or discard card if dragged completely out of initial top and bottom limits
+    if (newTop < -1 * rect.height) {
+      dispatchEvent(cardDropped(game, player, card));
+    } else if (newTop > rect.height) {
+      dispatchEvent(cardDropped(game, player, card)); // hide card
+    }
+    dragCancel();
+  }
+
+  element.addEventListener("mousedown", (event) => dragStart(event));
+  element.addEventListener("touchstart", (event) =>
+    dragStart(event.touches[0]),
+  );
+
+  element.addEventListener("mousemove", (event) => dragMove(event));
+  element.addEventListener("touchmove", (event) => dragMove(event.touches[0]));
+
+  element.addEventListener("mouseout", dragCancel);
+  element.addEventListener("touchcancel", dragCancel);
+
+  element.addEventListener("mouseup", dragEnd);
+  element.addEventListener("touchend", dragEnd);
 }
