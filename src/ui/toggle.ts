290602dd5591d843982e62e666ed0628a4efc1ea
Michel Sabchuk <michel@turbosys.com.br>
2024-08-24 12:41:20 -0300

    Remove debug statements

--- src/ui/toggle.ts
+++ src/ui/toggle.ts
@@ -9,10 +9,8 @@ export function renderToggle(
   const id = `tg-${getId()}`;
   setTimeout(() => {
     const button = getElement(id);
-    console.log("button", button);
     button.addEventListener("click", () => {
       const newState = onClick();
-      console.log("click", newState);
       button.classList.remove("on", "off");
       button.classList.add(newState ? "on" : "off");
     });
