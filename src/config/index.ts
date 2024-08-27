3cd3b3cad577bfb5483e20b734cd31e75c3037c2
Michel Sabchuk <michel@turbosys.com.br>
2024-08-27 11:49:50 -0300

    Create the official truco deck

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,19 +1,11 @@
-import { Card, Deck } from "@/core";
+import { Card, Deck, Suit } from "@/core";
 
 export const defaultDeck = new Deck(Array.from(generateDefaultCards()));
 
 function* generateDefaultCards() {
-  for (const cardNumber of range(13)) {
-    for (const suit of range(4)) {
+  for (const cardNumber of [3, 2, 1, 12, 11, 10, 7, 6, 5, 4]) {
+    for (const suit of [Suit.Clubs, Suit.Hearts, Suit.Spades, Suit.Diamonds]) {
       yield new Card(cardNumber, suit);
     }
-  }
-}
-
-function* range(count: number) {
-  let currentIndex = 1;
-  while (currentIndex <= count) {
-    yield currentIndex;
-    currentIndex++;
   }
 }
