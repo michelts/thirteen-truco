6cc2923f7641f77b54efa761f7cce6e1844cc0fe
Michel Sabchuk <michel@turbosys.com.br>
2024-08-29 17:07:54 -0300

    Fix default shuffle function

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,14 +1,14 @@
-export function shuffle<T>(items: T[]) {
-  const shuffleItems = [...items];
-  let currentIndex = items.length;
+export function shuffle<T>(originalItems: T[]) {
+  const shuffledItems = [...originalItems];
+  let currentIndex = shuffledItems.length;
   while (currentIndex !== 0) {
     const randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
     // swap positions
-    [items[currentIndex], items[randomIndex]] = [
-      items[randomIndex],
-      items[currentIndex],
+    [shuffledItems[currentIndex], shuffledItems[randomIndex]] = [
+      shuffledItems[randomIndex],
+      shuffledItems[currentIndex],
     ];
   }
-  return shuffleItems;
+  return shuffledItems;
 }
