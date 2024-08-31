438796822c1dd33b308c2be4c14ae4fcc6b75230
Michel Sabchuk <michel@turbosys.com.br>
2024-08-31 16:57:48 -0300

    Re-render score only if the round was acknowledge to avoid early feedback

--- src/ui/score.ts
+++ src/ui/score.ts
@@ -2,7 +2,7 @@ import type { Game } from "@/types";
 import { getElement } from "@/utils/elements";
 
 export function renderScore(game: Game) {
-  window.addEventListener("roundDone", (event) => {
+  window.addEventListener("roundAcknowledged", (event) => {
     redraw(event.detail.game);
   });
   return `<div id="scr">${render(game)}</div>`;
